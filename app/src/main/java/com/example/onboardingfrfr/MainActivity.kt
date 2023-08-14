8d3e0388efd98955591428bec3ad864399d0b346
Soufyan <tameur.merad@gmail.com>
2023-08-14 18:00:05 +0100

    init commit

---
+++ app/src/main/java/com/example/onboardingfrfr/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.onboardingfrfr
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webViewID)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://estin-guide-alpha.vercel.app/")
+        webView.settings.javaScriptEnabled = true
+
+
+    }
+
+    override fun onBackPressed() {
+        if (findViewById<WebView>(R.id.webViewID).canGoBack()){
+            findViewById<WebView>(R.id.webViewID).goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
